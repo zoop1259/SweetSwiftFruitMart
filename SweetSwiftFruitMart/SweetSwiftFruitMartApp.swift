4187b81ff468fe0c68ef293ef7dd76c794b0838b
zoop1259 <kdm432191@gmail.com>
2022-06-26 23:00:55 +0900

    Initial Commit

---
+++ SweetSwiftFruitMart/SweetSwiftFruitMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetSwiftFruitMartApp.swift
+//  SweetSwiftFruitMart
+//
+//  Created by 강대민 on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SweetSwiftFruitMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
